ee7e01dbe15c53c0f6f446d726a4d2f3949ab599
Aralcarim <vaclavkgfilip@gmail.com>
2026-01-25 14:58:41 +0000

    Update gallery: add new family photos, remove unused, sort chronologically

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     return (
         <footer style={{
             textAlign: 'center',
-            padding: '40px 0',
-            marginTop: '50px',
+            padding: '20px 0',
+            marginTop: '20px',
             fontSize: '0.9rem',
             color: '#666'
         }}>
